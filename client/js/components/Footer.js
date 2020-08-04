ff3ca24158f987e535fdad680ab53ce4aee26870
simonjv01 <sjvargas01@gmail.com>
2020-08-03 23:53:40 -0400

    footer updates

--- client/js/components/Footer.js
+++ client/js/components/Footer.js
@@ -1,13 +1,48 @@
 export default function Footer() {
     return `
-    <h3>&copy WeCanScrumIt 2020</h3>
+    
+    <footer id="footer">
+    <section>Re-Buy-Cycling
+    <br><br>
+    <ul id="footer-list-sections">
+        <li> Home </li>
+        <br>
+        <li> Our Mission </li>
+        <br>
+        <li> Join Our Team</li>
+        <br>
+        <li> Volunteer</li>
+        <br><br><br><br>
+        <form>
+        <p id="subscribe">Subscribe</p><p id="join">Join our newsletter to get
+        the latest information.
+        <input type="email" id="email" name="email">
+        <button type="submit">Subscribe </button> </p>
+        </form>
+        </section>
+    <section>Contact Us<br>
+    <br>
+    <p id="contact"> Phone: 614-987-0021</P>
+    <br>
+    <p id="contact"> info@rebuycycling.com</p>
+    <br><br><br><br><br><br>
     <ul class='social-list'>
         
-        <a href="#" class="fa fa-facebook"></a>
-     
-        <a href="#" class="fa fa-twitter"></a>
+    <a href="#" class="fa fa-facebook"></a>
+    <a href="#" class="fa fa-twitter"></a>
+    <a href="#" class="fa fa-instagram"></a>
+</ul></section>
+    <section>Our Address<br><br>
+    <p id="address"> 1234 Main St<br>
+        Columbus, OH 43026 </p>
+        <br>
+        <p id="address">View Map</p>
+        <br><br><br>
         
-        <a href="#" class="fa fa-instagram"></a>
-    </ul>
+        </section>
+
+    </footer>
+    
+ 
     `;
 }
