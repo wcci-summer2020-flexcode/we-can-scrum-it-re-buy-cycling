c3cd42a80544988d6a57e170b8976e7bc32b2ea3
simonjv01 <sjvargas01@gmail.com>
2020-08-06 14:20:13 -0400

    mission updates, volunteer form updates, sponsor updates on volunteer submit page

--- client/js/components/OurMission.js
+++ client/js/components/OurMission.js
@@ -1,14 +1,20 @@
+import missionbike1 from "../../assets/mission-bike1.jpg";
+import missionbike2 from "../../assets/mission-bike2.jpg";
+import missionbike3 from "../../assets/mission-bike3.jpg";
+
+
 
 export default function ourMission() {
   return `
     <h1>Our Mission</h1>
     
     <body>
-    <p>Our mission at ReBuycycle is to ease the burden on families by providing an easier way to replace bikes.  We understand the high cost that can be encountered when purchasing bikes year after year for young children that are out growing their bikes every year. </p><br>
+   
+    <p id="missionp1"> <img class="mission-image1" src="${missionbike1}" alt="bike">Our mission at ReBuycycle is to ease the burden on families by providing an easier way to replace bikes.  We understand the high cost that can be encountered when purchasing bikes year after year for young children that are out growing their bikes every year. </p><br><br>
     
-    <p>With that in mind, ReBuycyle is a resource for parents to utilize and lessen the cost of acquiring new bicycles for their children to enjoy.</p><br>
+    <p><img class="mission-image2" src="${missionbike2}" alt="bike">With that in mind, ReBuycyle is a resource for parents to utilize and lessen the cost of acquiring new bicycles for their children to enjoy.</p>
     <p>We believe that a child with a bicycle is an extremely important part of childhood. It is a fun activity that promotes having an active lifestyle, encourages children to enjoy the outdoors, and it can give a child confidence to pursue new adventures.</p><br>
-    <p>"As a kid I had a dream - I wanted to own my own bicycle. When I got the bike I must have been the happiest boy in Liverpool, maybe the world. I lived for that bike. Most kids left their bike in the backyard at night. Not me. I insisted on taking mine indoors and the first night I even kept it in my bed." - John Lennon, British musician
+    <p><img class="mission-image3" src="${missionbike3}" alt="bike">"As a kid I had a dream - I wanted to own my own bicycle. When I got the bike I must have been the happiest boy in Liverpool, maybe the world. I lived for that bike. Most kids left their bike in the backyard at night. Not me. I insisted on taking mine indoors and the first night I even kept it in my bed." - John Lennon, British musician
    </body>
    `;
 }
--- client/js/components/Volunteer.js
+++ client/js/components/Volunteer.js
@@ -8,40 +8,40 @@ export default function navVolunteer() {
 <div class="volunteer-title"><h1>Volunteer Registration</h1></div>
 <div class="main">
 <form id="volunteer">
-<div id="name">
+<div class="form-div">
 
 
-<label class="firstlabel">First Name</label>
-<input id="fname" type="text" name="first_name"><br>
-<label class="secondlabel">Last Name</label>
-<input class="lname" type="text" name="last_name"><br>
-<label class="thirdlabel">Address</label>
-<input class="address" type="text" name="address"><br>
-<label class="fourthlabel">City</label>
-<input class="city" type="text" name="city"><br>
-<label class="fifthlabel">Zip</label>
-<input class="zip" type="text" name="zip"><br>
-<label class="sixthlabel">Phone</label>
-<input class="phone" type="text" name="phone"><br>
-<label class="seventhlabel">Email</label>
-<input class="email" type="text" name="email"><br>
-<label class="eighthlabel">Employer</label>
-<input class="employer" type="text" name="employer"><br>
-<label class="ninthlabel">Position</label>
-<input class="position" type="text" name="position"><br><br>
+<label class="label">First Name</label>
+<input class="name" type="text" name="first_name"><br>
+<label class="label">Last Name</label>
+<input class="name" type="text" name="last_name"><br>
+<label class="label">Address</label>
+<input class="name" type="text" name="address"><br>
+<label class="label">City</label>
+<input class="name" type="text" name="city"><br>
+<label class="label">Zip</label>
+<input class="name" type="text" name="zip"><br>
+<label class="label">Phone</label>
+<input class="name" type="text" name="phone"><br>
+<label class="label">Email</label>
+<input class="name" type="text" name="email"><br>
+<label class="label">Employer</label>
+<input class="name" type="text" name="employer"><br>
+<label class="label">Position</label>
+<input class="name" type="text" name="position"><br><br>
 <div id="weekdays">
 <p class="days">Days Available</p>
-<input type="checkbox" id="monday" name="monday" value="Monday">
-<label for="monday"> Monday</label>
-<input type="checkbox" id="tuesday" name="tuesday" value="tuesday">
-<label for="tuesday">Tuesday</label>
-<input type="checkbox" id="wednesday" name="wednesday" value="wednesday">
-<label for="wednesday">Wednesday</label>
-<input type="checkbox" id="thursday"
+<input type="checkbox" id="weekday" name="monday" value="Monday">
+<label id="weekday"> Monday</label>
+<input type="checkbox" id="weekday" name="tuesday" value="tuesday">
+<label id="weekday">Tuesday</label>
+<input type="checkbox" id="weekday" name="wednesday" value="wednesday">
+<label id="weekday">Wednesday</label>
+<input type="checkbox" id="weekday"
 name="thursday" value="thursday"> 
-<label for="thursday">Thursday</label>
-<input type="checkbox" id="friday" name="friday" value="friday">
-<label for="friday">Friday</label><br>
+<label id="weekday">Thursday</label>
+<input type="checkbox"  name="friday" value="friday">
+<label id="weekday">Friday</label><br>
 <p>Time Availability</p>
 <textarea rows="5" cols="50" name="description">
 Provide details on hours available</textarea><br><br>
